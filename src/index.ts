0697dce0705ba637f84ad07d0f48268cd3d01426
Amit Kumar <15000amitkumar@gmail.com>
2026-02-26 19:07:16 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express from 'express'
+import client from 'prom-client'
+import { requestCount } from './requestCounter';
+const app = express();
+
+// app.use(calTime)
+
+// // @ts-ignore
+// function calTime(req,res,next){
+//     const st = Date.now();
+
+//     next();
+
+//     const ed = Date.now();
+
+//     console.log("It took ", ed-st, 'ms');
+// }
+
+app.use(requestCount)
+
+
+app.get('/user', (req,res)=>{
+    res.json({
+        name:"Amit Maurya"
+    })
+})
+
+app.get('/metrics', async(req,res)=>{
+    const metrics = await client.register.metrics();
+    res.set('Content-Type',client.register.contentType);
+    res.end(metrics)
+})
+
+app.listen(3000, ()=>{
+    console.log("Server Running on Port 3000")
+})
